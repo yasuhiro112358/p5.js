d4e8c0aa0419aee8883fcdeb1fb39357a17e4d44
Yasuhiro WATANABE <info@nabeyasu.com>
2024-10-05 20:25:08 +0900

    Update sketch.js to create a canvas, set background color, and draw a moving rectangle

--- src/sketch.js
+++ src/sketch.js
@@ -1,14 +1,32 @@
 import p5 from 'p5';
 
-const sketch = (p) => {
+const sketch = (p) => { // p could be any variable name
   p.setup = () => {
-    p.createCanvas(400, 400);
+    const canvas = p.createCanvas(480, 240); // create a canvas
+    canvas.parent('#canvas-container'); // attach the canvas to a div with id 'canvas
+
+    // p.background(127); // 0-255 (black-white) 
+    // p.background(255, 0, 0); // RGB
+    // p.background(255, 0, 0, 50); // RGBA
+    
+    p.colorMode(p.HSB); // set the color mode to HSB
+    p.background(180, 100, 100); // HSB color
+    
+    // p.background('#ff0000'); // hex color
+    // p.background('red'); // color name
+    // p.background('rgb(255, 0, 0)'); // color string
+    
+    p.frameRate(60); // set the frame rate (FPS)
   };
 
-  p.draw = () => {
-    p.background(0); // set the background to black
-    p.rect(0, 40, 100,50); // draw a rectangle
+  let x = 0; // declare a variable to store the x-coordinate of the rectangle 
+  p.draw = () => { // p5.js calls this function 60 times per second by default 
+    // p.background(0); // set the background to black
+    // p.rect(40, 40, 50, 50); // draw a rectangle
+
+    p.rect(x, 40, 50, 50); // draw a rectangle at the x-coordinate
+    x++;
   };
 };
 
-new p5(sketch);
+new p5(sketch); // create a new instance of p5 with the sketch function
