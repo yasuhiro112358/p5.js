5cbbc5d475364b40d4567c7a8ca8bff4a33ec90f
Yasuhiro WATANABE <info@nabeyasu.com>
2024-10-05 18:44:35 +0900

    Update script source file and background color in index.html and sketch.js

--- src/sketch.js
+++ src/sketch.js
@@ -1,16 +1,13 @@
 import p5 from 'p5';
 
-
-console.log('Hello, world!');
-
-
 const sketch = (p) => {
   p.setup = () => {
     p.createCanvas(400, 400);
   };
 
   p.draw = () => {
-    p.background(220);
+    p.background(0); // set the background to black
+    p.rect(0, 40, 100,50); // draw a rectangle
   };
 };
 
